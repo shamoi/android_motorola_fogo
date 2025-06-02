c6ecca1a62212df891a0bdf7f55d091a1937b7a3
kay6888 <kaynikko88@gmail.com>
2025-06-02 01:33:29 -0500

    Update BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -24,18 +24,10 @@
 # components.
 
 # Recovery
-TARGET_OTA_ASSERT_DEVICE := eqe
+TARGET_OTA_ASSERT_DEVICE := fogo
 
 # TWRP specific build flags
-TW_FRAMERATE := 120
+# TW_FRAMERATE := 120
 
-TW_MAX_BRIGHTNESS := 2000
 
-# Vibrator
-TW_SUPPORT_INPUT_AIDL_HAPTICS := true
-TW_SUPPORT_INPUT_AIDL_HAPTICS_FIX_OFF := true
 
-TARGET_RECOVERY_DEVICE_MODULES += libexpat android.hardware.vibrator-V2-ndk
-RECOVERY_LIBRARY_SOURCE_FILES += \
-     $(TARGET_OUT_SHARED_LIBRARIES)/libexpat.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.vibrator-V2-ndk.so
