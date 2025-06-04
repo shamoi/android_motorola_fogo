22ee8518af2f0a36f4632f1592b64c1559a52391
kay6888 <kaynikko88@gmail.com>
2025-06-04 01:52:27 -0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,8 +6,8 @@
 #
 
 export ALLOW_MISSING_DEPENDENCIES=true
-
- 
+export BUILD_BROKEN_DUP_RULES=true
+export BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES=true
 
 add_lunch_combo twrp_fogo-user
 add_lunch_combo twrp_fogo-userdebug
