20c9ebaa94b4f28d0b490c2e0d576201cc0a1eb3
kay6888 <kaynikko88@gmail.com>
2025-06-04 04:17:20 -0500

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,9 +20,6 @@ export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_DUP_RULES=true
 export BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES=true
 export TW_HAS_NO_RECOVERY_PARTITION=true
-export add_lunch_combo='twrp_fogo-user'
-export add_lunch_combo='twrp_fogo-userdebug'
-export add_lunch_combo='twrp_fogo-eng'   
 
 # SDK
 BOARD_SYSTEMSDK_VERSIONS := 34
